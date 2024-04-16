b6440605ae0d7a0f91f1799ab0350e0a1f6e7fd5
MehdiMahmoudico <mmeehdi.mma@gmail.com>
2024-04-17 00:14:18 +0100

    likes

---
+++ Web Fundamentals/JS/Core/Likes/script1.js
@@ -0,0 +1,5 @@
+
+var i=3;
+function boxnu(element){
+    document.querySelector('.box').innerHTML = ++i;
+}
---
+++ Web Fundamentals/JS/Core/Likes/script2.js
@@ -0,0 +1,15 @@
+var i=9;
+function boxnu(element){
+    
+    document.querySelector('.box').innerHTML = ++i;
+}
+
+var e=12;
+function boxnues(element){
+    document.querySelector('.boxe1').innerHTML = ++e;
+}
+
+var u=9;
+function boxnue(element){
+    document.querySelector('.boxe2').innerHTML = ++u;
+}
