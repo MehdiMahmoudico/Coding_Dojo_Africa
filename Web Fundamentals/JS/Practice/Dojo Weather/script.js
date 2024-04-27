9560260e21e17cb78f8840a42e5b71084312626d
MehdiMahmoudico <mmeehdi.mma@gmail.com>
2024-04-27 19:15:45 +0100

    video preview

---
+++ Web Fundamentals/JS/Practice/Dojo Weather/script.js
@@ -0,0 +1,8 @@
+var start = 0;
+var end = 10;
+    
+while(start <= end) {
+    console.log("start: " + start + ", end: " + end);
+    start++;
+    end--;
+}
---
+++ Web Fundamentals/JS/Practice/Video Preview/script.js
@@ -0,0 +1,7 @@
+var clip = document.querySelector(".clip")
+    function playvideo(clip) {
+    clip.play();
+    }  
+function pausevideo(clip) {
+    clip.pause();
+    }   
